d9515ac62645fd9fcb28b61ee1a9e1a9d45fbd9b
Rano Kuhl <ranokuhl@gmail.com>
2023-07-23 22:20:08 +0200

    tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="uppercase text-indigo-500 text=6xl">
+    Hello Peoples of Earth....
+</h1>
