2cf81e81d9bdb00e3e2bec60413a8808d6af2117
Rano Kuhl <ranokuhl@gmail.com>
2023-07-23 22:21:03 +0200

    :

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<main>
+    <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <h1 class="uppercase text-indigo-500 text=6xl">
     Hello Peoples of Earth....
 </h1>
+</main>
+
+
